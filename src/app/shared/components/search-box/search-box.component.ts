725ce92090520a911182a30c66e41c86f7988a9b
nico-ortiz <nicortiz1234@gmail.com>
2024-05-14 11:03:45 -0300

    create search country box component

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-search-box',
+  selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css'
 })
 export class SearchBoxComponent {
 
+  @ViewChild('txtInput')
+  txtInput! : ElementRef<HTMLInputElement>;
+
+  @Output()
+  public onValue : EventEmitter<string> = new EventEmitter();
+
+  public sendCountry(country: string): void {
+    this.onValue.emit(country);
+    console.log(country);
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -10,8 +10,8 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     HeaderComponent,
+    HomePageComponent,
     SearchBoxComponent,
-    HomePageComponent
   ],
   imports: [
     CommonModule,
