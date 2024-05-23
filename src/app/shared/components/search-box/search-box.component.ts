c37d955760ba0b3de18e56364f5c6ffabddd30df
nico-ortiz <nicortiz1234@gmail.com>
2024-05-23 17:27:39 -0300

    add debouncer to read input for search box component

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,18 +1,22 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
+import { Subject, Subscription, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
   styleUrl: './search-box.component.css'
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit, OnDestroy {
+
+  private debouncer: Subject<string> = new Subject<string>();
+  private debouncerSuscription?: Subscription;
 
   @ViewChild('txtInput')
   txtInput! : ElementRef<HTMLInputElement>;
 
   @Output()
-  public onValue : EventEmitter<string> = new EventEmitter();
+  public onDebounce : EventEmitter<string> = new EventEmitter();
 
   public isComponentDark!: string;
 
@@ -20,8 +24,21 @@ export class SearchBoxComponent {
     sharedService.mode.subscribe(mode => this.isComponentDark = mode)
   }
 
+  ngOnInit(): void {
+    this.debouncerSuscription = this.debouncer
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value => {
+      this.onDebounce.emit(value);
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.debouncerSuscription?.unsubscribe();
+  }
+
   public sendCountry(country: string): void {
-    this.onValue.emit(country);
-    console.log(country);
+    this.debouncer.next(country);
   }
 }
